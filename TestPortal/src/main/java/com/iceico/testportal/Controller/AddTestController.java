0ea4c923f442f7e6b753046d013b4b56434b5354
NextGen-Coder <nextgenerationcoder@gmail.com>
2020-03-05 17:08:37 +0530

    Add test added

--- TestPortal/src/main/java/com/iceico/testportal/Controller/AddTestController.java
+++ TestPortal/src/main/java/com/iceico/testportal/Controller/AddTestController.java
@@ -3,10 +3,16 @@
  */
 package com.iceico.testportal.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.validation.Valid;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,9 +22,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.iceico.testportal.Exceptions.ResourceNotFoundException;
 import com.iceico.testportal.Model.AddTest;
+import com.iceico.testportal.Model.QuestionBank;
 import com.iceico.testportal.Service.AddTestService;
+import com.iceico.testportal.Service.QuestionBankService;
 import com.iceico.testportal.Service.SubjectService;
 import com.iceico.testportal.Service.UserService;
 
@@ -41,6 +51,9 @@ public class AddTestController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private QuestionBankService questionBankService;
 
 	@GetMapping("/admin/add/test")
 	public String getTest(ModelMap modelMap, Locale locale) {
@@ -51,8 +64,8 @@ public class AddTestController {
 	}
 
 	@PostMapping("/admin/add/test/save")
-	public String saveTest(@ModelAttribute("addTest") @Valid AddTest addTest, BindingResult bindingResult,
-			ModelMap modelMap, Locale locale) {
+	public String saveTest(@RequestParam("questionsJson") String questions, @ModelAttribute("addTest") @Valid AddTest addTest, BindingResult bindingResult,
+			ModelMap modelMap, Locale locale) throws ParseException, ResourceNotFoundException {
 		if (bindingResult.hasErrors()) {
 			modelMap.addAttribute("addTest", new AddTest());
 			modelMap.addAttribute("subjectList", this.subjectService.getSubjectList());
@@ -61,15 +74,28 @@ public class AddTestController {
 			System.out.println("***********" + bindingResult.getAllErrors());
 			return "addTest";
 		} else {
+			System.out.println("questions ========>"+questions);
 			System.out.println("====================>Saved!!!");
+			
+			List <QuestionBank> addedQuestions = new ArrayList<QuestionBank>();
+			
+			JSONParser parser = new JSONParser();
+			JSONArray questionArray = (JSONArray) parser.parse(questions);
+			
+			for (int i=0; i<questionArray.size(); i++) {
+				JSONObject questionIdObj = (JSONObject) questionArray.get(i);
+				Long questionId = Long.parseLong( questionIdObj.get("questionId")+"");
+				QuestionBank question = questionBankService.getQuestionBankById( questionId);
+				addedQuestions.add(question);
+			}
+			
+			addTest.setQuestionBank( addedQuestions);
+			
 			this.addTestService.saveAddTest(addTest);
 			modelMap.addAttribute("user", userService.findBySSO(this.getPrincipal()));
 			return "redirect:/admin/add/test";
 		}
 	}
-	
-	
-	
 
 	private String getPrincipal() {
 		String userName = null;
